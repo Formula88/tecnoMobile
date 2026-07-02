e01e52142c6593dc489c85da1f25aaca206c9ec3
Formula <ali88elsa96@gmail.com>
2026-07-02 16:45:52 +0330

    Designing the login UI/UX

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -4,14 +4,9 @@ import styles from "./LoginForm.module.scss";
 function LoginForm({ register, error }) {
   return (
     <>
-      <Inputs register={register} error={error} option={{}} name={"userName"} label={"نام کاربری"} placeholder={""} type="password"/>
-      <Inputs register={register} error={error} option={{}} name={"userName"} label={"نام کاربری"} placeholder={""} type="password"/>
-      <div className={styles.box}>
-        <div className={styles.right}>
-          
-        </div>
-        <div className={styles.left}></div>
-      </div>
+      <Inputs register={register} error={error} option={{required: "لطفا نام کاربری خود را وارد کنید"}} name={"userName"} label={"نام کاربری"} placeholder={"نام کاربری خود را وارد کنید"} type="text"/>
+      <Inputs register={register} error={error} option={{required: "لطفا رمز عبور خود را وارد کنید"}} name={"userPassword"} label={"رمز عبور"} placeholder={"رمز عبور خود را وارد کنید"} type="password"/>
+      
     </>
   );
 }
--- src/pages/Auth/Auth.jsx
+++ src/pages/Auth/Auth.jsx
@@ -2,7 +2,8 @@ import styles from "./Auth.module.scss";
 import { useState } from "react";
 import { useForm } from "react-hook-form";
 import LoginForm from "../../components/LoginForm/LoginForm";
-import logo from "../../assets/img/logo.svg"
+import logo from "../../assets/img/logo.svg";
+import { Link } from "react-router-dom";
 
 function Auth() {
   const {
@@ -13,17 +14,34 @@ function Auth() {
 
   const [page, setPage] = useState("login");
 
+  const pageToSignUp = () => {
+    setPage("signUp");
+  };
+  const pageToLogin = () => {
+    setPage("login");
+  };
+
   const onsubmit = (data) => {
     console.log(data);
   };
 
-  let btnText
+  let btnText, labelLink, linkText, funLink;
 
   switch (page) {
     case "login":
-      btnText = "ورود"
+      btnText = "ورود";
+      labelLink = "آیا حساب کاربری ندارید؟";
+      linkText = "ساخت حساب";
+      funLink = pageToSignUp;
       break;
-  
+
+    case "signUp":
+      btnText = "ثبت نام";
+      labelLink = "آیا حساب کاربری دارید؟";
+      linkText = "ورود به حساب";
+      funLink = pageToLogin;
+      break;
+
     default:
       break;
   }
@@ -32,10 +50,25 @@ function Auth() {
     <div className={styles.auth}>
       <img src={logo} alt="tecnomobile" className={styles.logo} />
       <form onSubmit={handleSubmit(onsubmit)} className={styles.form}>
-        {
-          page === "login" && (<LoginForm register={register} error={errors} />) 
-        }
-        <input type="submit" value={btnText} className={`btnPrimary ${styles.btn}`}/>
+        {page === "login" && <LoginForm register={register} error={errors} />}
+        <input
+          type="submit"
+          value={btnText}
+          className={`btnPrimary ${styles.btn}`}
+        />
+        <div className={styles.box}>
+          <div className={styles.right}>
+            <span className={styles.label}>{labelLink}</span>
+            <span className={`btn btn-link ${styles.link}`} onClick={funLink}>
+              {linkText}
+            </span>
+          </div>
+          <div className={styles.left}>
+            <Link to="/" className="btn btn-link">
+              بازگشت به صفحه اصلی
+            </Link>
+          </div>
+        </div>
       </form>
     </div>
   );
