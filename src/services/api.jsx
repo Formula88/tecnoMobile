6e6091cabb01f4169359823794d863dee3f79ad3
Formula <ali88elsa96@gmail.com>
2026-07-02 15:22:42 +0330

    add api.jsx

---
+++ src/services/api.jsx
@@ -0,0 +1,32 @@
+import axios from "axios";
+
+const client = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
+});
+
+export const GetItemInHome = async () => {
+  try {
+    const { data } = await client.get("api/showInHome");
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const GetProduct = async (id) => {
+  try {
+    const { data } = await client.get(`api/product/${id}`);
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const GetServices = async (page) => {
+  try {
+    const { data } = await client.get(`api/service/${page}`);
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
