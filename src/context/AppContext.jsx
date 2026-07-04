001f7bd059a1543f6eaea0ae6f19873333ebbe0c
Formula <ali88elsa96@gmail.com>
2026-07-04 18:48:39 +0330

    debug context and add userType

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -3,8 +3,9 @@ import { createContext, useState } from "react";
 export const Context = createContext();
 
 function AppContext({ children }) {
-  const [isLogin, setIsLogin] = useState(true);
-  return <Context.Provider value={isLogin}>{children}</Context.Provider>;
+  const [isLogin, setIsLogin] = useState(false);
+  const [userType, setUserType] = useState("public");
+  return <Context.Provider value={{isLogin,setIsLogin,userType,setUserType}}>{children}</Context.Provider>;
 }
 
 export default AppContext;
