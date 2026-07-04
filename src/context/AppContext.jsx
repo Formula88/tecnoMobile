deabc853a3f8c1918e531fc0d6bf9265bc778914
Formula <ali88elsa96@gmail.com>
2026-07-04 18:31:41 +0330

    create routes

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -3,7 +3,7 @@ import { createContext, useState } from "react";
 export const Context = createContext();
 
 function AppContext({ children }) {
-  const [isLogin, setIsLogin] = useState(false);
+  const [isLogin, setIsLogin] = useState(true);
   return <Context.Provider value={isLogin}>{children}</Context.Provider>;
 }
 
