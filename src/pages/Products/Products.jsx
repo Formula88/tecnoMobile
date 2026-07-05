4b58426d165c38847f8f2447aab9bbc1540814d6
Formula <ali88elsa96@gmail.com>
2026-07-05 13:44:11 +0330

    Add pagination to the products page

--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -1,12 +1,26 @@
 import styles from "./Products.module.scss";
 import HeaderNav from "../../components/layout/HeaderNav/HeaderNav";
 import Footer from "../../components/layout/Footer/Footer";
+import Pagination from "../../components/ui/Pagination/Pagination";
+import { useEffect, useState } from "react";
 
 function Products() {
+  const [pageCount, setPageCount] = useState(20);
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const handlePageChange = (event) => {
+    const selectedPage = event.selected + 1;
+    setCurrentPage(selectedPage);
+  };
+
+  useEffect(() => {
+
+  }, [currentPage]);
+
   return (
     <>
       <HeaderNav />
-      
+      <Pagination pageCount={pageCount} onPageChange={handlePageChange} />
       <Footer />
     </>
   );
