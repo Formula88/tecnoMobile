8d4d93b64e60e727803977821e0c382b74e6210e
Formula <ali88elsa96@gmail.com>
2026-07-05 15:52:55 +0330

    create search box

--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -3,23 +3,40 @@ import HeaderNav from "../../components/layout/HeaderNav/HeaderNav";
 import Footer from "../../components/layout/Footer/Footer";
 import Pagination from "../../components/ui/Pagination/Pagination";
 import { useEffect, useState } from "react";
+import SearchBox from "../../components/ui/SearchBox/SearchBox";
 
 function Products() {
   const [pageCount, setPageCount] = useState(20);
   const [currentPage, setCurrentPage] = useState(1);
+  const [searchValue, setSearchValue] = useState("");
+
+  const handleSearchSubmit = (e) => {
+    e.preventDefault();
+    console.log(searchValue);
+  };
+
+  const handleSearchChange = (e) => {
+    setSearchValue(e.currentTarget.value);
+  };
 
   const handlePageChange = (event) => {
     const selectedPage = event.selected + 1;
     setCurrentPage(selectedPage);
   };
 
-  useEffect(() => {
-
-  }, [currentPage]);
+  useEffect(() => {}, [currentPage]);
 
   return (
     <>
       <HeaderNav />
+      <div className={styles.SearchBox}>
+        <SearchBox
+          placeholder={"جستجوی بین محصولات..."}
+          onChange={handleSearchChange}
+          onSubmit={handleSearchSubmit}
+          value={searchValue}
+        />
+      </div>
       <Pagination pageCount={pageCount} onPageChange={handlePageChange} />
       <Footer />
     </>
