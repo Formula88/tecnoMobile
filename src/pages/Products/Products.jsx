7a144cfdcdcdab61a0c3ee4dd0e1719c4f0438a4
Formula <ali88elsa96@gmail.com>
2026-07-05 17:59:00 +0330

    Design product cards

--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -4,11 +4,13 @@ import Footer from "../../components/layout/Footer/Footer";
 import Pagination from "../../components/ui/Pagination/Pagination";
 import { useEffect, useState } from "react";
 import SearchBox from "../../components/ui/SearchBox/SearchBox";
+import ProductsCard from "../../components/cards/ProductsCard/ProductsCard";
 
 function Products() {
   const [pageCount, setPageCount] = useState(20);
   const [currentPage, setCurrentPage] = useState(1);
   const [searchValue, setSearchValue] = useState("");
+  const [products, setProducts] = useState([]);
 
   const handleSearchSubmit = (e) => {
     e.preventDefault();
@@ -29,15 +31,37 @@ function Products() {
   return (
     <>
       <HeaderNav />
-      <div className={styles.SearchBox}>
-        <SearchBox
-          placeholder={"جستجوی بین محصولات..."}
-          onChange={handleSearchChange}
-          onSubmit={handleSearchSubmit}
-          value={searchValue}
-        />
+      <div className={styles.products}>
+        <div className={styles.SearchBox}>
+          <SearchBox
+            placeholder={"جستجوی بین محصولات..."}
+            onChange={handleSearchChange}
+            onSubmit={handleSearchSubmit}
+            value={searchValue}
+          />
+        </div>
+        <div className="container">
+          <div
+            className={`row row-cols-lg-4 row-cols-sm-2 row-cols-1 ${styles.productsCards}`}
+          >
+            {products.map((value) => {
+              return (
+                <div className={`col ${styles.col}`}>
+                  <ProductsCard
+                    id={}
+                    title={}
+                    description={}
+                    img={}
+                    price={}
+                    discount={}
+                  />
+                </div>
+              );
+            })}
+          </div>
+        </div>
+        <Pagination pageCount={pageCount} onPageChange={handlePageChange} />
       </div>
-      <Pagination pageCount={pageCount} onPageChange={handlePageChange} />
       <Footer />
     </>
   );
