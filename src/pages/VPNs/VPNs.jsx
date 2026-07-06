2d27d59da626f2163d5592f08e4083ba41a1baa9
Formula <ali88elsa96@gmail.com>
2026-07-06 22:06:44 +0330

    Improve the VPN page intro UI

--- src/pages/VPNs/VPNs.jsx
+++ src/pages/VPNs/VPNs.jsx
@@ -1,10 +1,12 @@
 import HeaderNav from "../../components/layout/HeaderNav/HeaderNav";
 import Footer from "../../components/layout/Footer/Footer";
+import IntroVPN from "../../components/sections/IntroVPN/IntroVPN";
 
 function VPNs() {
   return (
     <>
       <HeaderNav />
+      <IntroVPN />
       <Footer />
     </>
   );
