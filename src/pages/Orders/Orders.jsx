6a994a565aa44b500da0fe90ce2a1af8fa21f8f0
Formula <ali88elsa96@gmail.com>
2026-07-22 16:36:39 +0330

    Add order card

--- src/pages/Orders/Orders.jsx
+++ src/pages/Orders/Orders.jsx
@@ -1,3 +1,4 @@
+import OrderCard from "../../components/cards/OrderCard/OrderCard";
 import Footer from "../../components/layout/Footer/Footer";
 import HeaderNav from "../../components/layout/HeaderNav/HeaderNav";
 import styles from "./Orders.module.scss";
@@ -8,6 +9,13 @@ function Orders() {
       <HeaderNav />
       <div className={styles.orders}>
         <h1 className={styles.title}>سفارشات شما</h1>
+        <div className="container">
+          <div className={styles.cards}>
+            <OrderCard status={true}/>
+            <OrderCard status={true}/>
+            <OrderCard status={false}/>
+          </div>
+        </div>
       </div>
       <Footer />
     </>
